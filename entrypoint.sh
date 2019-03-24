73a6212eceb372daae7d107087e24ad57c0e061a
Pascal Lauener <pascal@lauener.email>
2019-03-24 14:09:32 +0100

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,50 +1,6 @@
-!/bin/bash
+#!/usr/bin/env sh
 
 set -ex
-
-write_key() {
-	mkdir -p "${JENKINS_AGENT_HOME}/.ssh"
-	echo "$1" > "${JENKINS_AGENT_HOME}/.ssh/authorized_keys"
-	chown -Rf jenkins:jenkins "${JENKINS_AGENT_HOME}/.ssh"
-	chmod 0700 -R "${JENKINS_AGENT_HOME}/.ssh"
-}
-
-if [[ $JENKINS_SLAVE_SSH_PUBKEY == ssh-* ]]; then
-  write_key "${JENKINS_SLAVE_SSH_PUBKEY}"
-fi
-if [[ $# -gt 0 ]]; then
-  if [[ $1 == ssh-* ]]; then
-    write_key "$1"
-    shift 1
-  else
-    ssh-keygen -A
-    "$@" & #-E /var/log/sshd.log
-  fi
-fi
-
-# ensure variables passed to docker container are also exposed to ssh sessions
-env | grep _ >> /etc/environment
-
-ssh-keygen -A
-set -- /usr/sbin/sshd -e -D "${@}"
-
-#exec "${@}"
-"${@}" &
-
-exec "$(which dind)" dockerd \
-      --host=unix:///var/run/docker.sock \
-      --insecure-registry registry.service.lidop.local:5000 \
-      --host=tcp://0.0.0.0:2375 
-
-
-
-
-
-#!/usr/bin/env sh
-
-
-#!/usr/bin/env sh
-
 # The MIT License
 #
 #  Copyright (c) 2015, CloudBees, Inc.
